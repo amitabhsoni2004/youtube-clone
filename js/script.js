c4b11c40e22e9f2b8d64ee6c73672ffc9142f672
Amitabh Soni <144244954+amitabhsoni2004@users.noreply.github.com>
2024-04-09 00:17:03 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -19,6 +19,44 @@ window.addEventListener('load', function() {
 });
 
 
+document.querySelector(".hamburger").addEventListener("click", () => {
+    let paragraphs = document.getElementsByTagName("p");
+    let headings = document.getElementsByTagName("h3");
+    let videoFrame =  document.querySelectorAll(".videoFrame")
+    // Toggle paragraphs visibility
+    for (let i = 0; i < paragraphs.length; i++) {
+        if (paragraphs[i].style.display === "none") {
+            paragraphs[i].style.display = "block";
+            document.querySelector(".left").style.width=`15vw`
+            document.querySelector(".left").style.border = "none"
+            videoFrame.forEach((e)=>{
+                e.style.height=`215px`
+               })
+        } else {
+            paragraphs[i].style.display = "none";
+            document.querySelector(".left").style.width=`3.5%`
+            document.querySelector(".right").style.width=`96.5%`
+           videoFrame.forEach((e)=>{
+            e.style.height=`240px`
+           })
+            document.querySelector(".left").style.border = "0.1px solid gray"
+        }
+    }
+    
+    // Toggle headings visibility
+    for (let i = 0; i < headings.length; i++) {
+        if (headings[i].style.display === "none") {
+            headings[i].style.display = "block";
+            document.querySelector(".left").style.width=`15vw`
+        } else {
+            headings[i].style.display = "none";
+            document.querySelector(".left").style.width=`3.5%`
+        }
+    }
+});
+
+
+
 // let createsvg = document.querySelector(".createsvg");
 // createsvg.addEventListener("click",()=>{
 //     createsvg.style.filter = "invert(100%)";
