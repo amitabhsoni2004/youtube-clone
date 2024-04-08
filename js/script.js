ac49bdd0ab631ca114c7439283ca1a3c8be298ca
Amitabh Soni <144244954+amitabhsoni2004@users.noreply.github.com>
2024-04-08 21:30:08 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-document.querySelector(".hamburger").addEventListener("click",()=>{
+document.querySelector(".hamburger1").addEventListener("click",()=>{
     document.querySelector(".left").style.left = 0;
+    document.querySelector(".left").style.width = `70%`
 })
 document.querySelector(".close").addEventListener("click",()=>{
     document.querySelector(".left").style.left = `-120%` ;
 })
 
+let notify = document.querySelector(".notify");
+notify.addEventListener("click",()=>{
+    notify.style.filter = "invert(100%)";
+})
+// let createsvg = document.querySelector(".createsvg");
+// createsvg.addEventListener("click",()=>{
+//     createsvg.style.filter = "invert(100%)";
+// })
+// let account = document.querySelector(".account");
+// account.addEventListener("click",()=>{
+//     account.style.filter = "invert(100%)";
+// })
+// let searchsvg1 = document.querySelector(".searchsvg1");
+// searchsvg1.addEventListener("click",()=>{
+//     searchsvg1.style.filter = "invert(100%)";
+// })
